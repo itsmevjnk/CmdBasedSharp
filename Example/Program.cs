4ae82fa5db5f41389db8e8bb0e296ab15341a62e
Vinh Nguyen <ngtv0404@gmail.com>
2025-10-03 18:20:33 +1000

    add MVP example

--- Example/Program.cs
+++ Example/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace CmdBased
+{
+    class Example
+    {
+        public static void Main()
+        {
+            CommandScheduler scheduler = new();
+
+            Gamepad gamepad = new(); // default device is /dev/input/js0
+
+            scheduler.RegisterTriggers
+            (
+                gamepad.Triggers[(int)Gamepad.ButtonIDs.TRIANGLE]
+                .OnTrue
+                (
+                    new InstantCommand(() => { Console.WriteLine("Triangle pressed"); })
+                )
+            );
+
+            while (true)
+            {
+                scheduler.Periodic();
+                Thread.Sleep(20);
+            }
+        }
+    }
+}
