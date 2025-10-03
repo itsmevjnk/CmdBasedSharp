b817c95e194c3df4d50614675f4ec4c9be1da8cc
Vinh Nguyen <ngtv0404@gmail.com>
2025-10-03 23:11:32 +1000

    add test motor in, as well as example that overrides default drive command

--- Example/Program.cs
+++ Example/Program.cs
@@ -10,30 +10,27 @@
 
         /* subsystems */
         static DriveSubsystem DriveSub = new();
+        static MotorSubsystem Motor1 = new("Motor1");
 
         static readonly double JSTICK_MAX_SPEED = 2.0 * TICK_TIME_SEC;
         // maximum drive velocity from joystick input
 
         public static void PrintStatus()
         {
-            Console.WriteLine(DriveSub); // TODO
+            Console.WriteLine(
+                $"{DriveSub}, {Motor1}"
+            );
         }
 
         public static void Main()
         {
             /* register subsystems */
             Scheduler.RegisterSubsystem(DriveSub);
+            Scheduler.RegisterSubsystem(Motor1);
 
             using (var gamepad = new Gamepad()) // default to /dev/input/js0
             {
-                Scheduler.RegisterTriggers
-                (
-                    gamepad.Triggers[(int)Gamepad.ButtonIDs.TRIANGLE]
-                    .OnTrue
-                    (
-                        new InstantCommand(() => { Console.WriteLine("Triangle pressed"); })
-                    )
-                );
+                gamepad.RegisterTriggers(Scheduler); // register all buttons
 
                 /* drivetrain default command: joystick teleoperation */
                 DriveSub.DefaultCommand = new RunCommand(() =>
@@ -44,6 +41,40 @@
                         xInput * JSTICK_MAX_SPEED, yInput * JSTICK_MAX_SPEED
                     );
                 }, [DriveSub]);
+
+                /* press TRIANGLE to run Motor1 for 1 sec */
+                gamepad.Triggers[(int)Gamepad.ButtonIDs.TRIANGLE].OnTrue(
+                    Motor1.RunStopCommand(1.0)
+                        .WithTimeout((int)(1 / TICK_TIME_SEC))
+                );
+
+                /* press CIRCLE to do the same, but then reverse when rel'd */
+                gamepad.Triggers[(int)Gamepad.ButtonIDs.CIRCLE].OnTrue(
+                    Motor1.RunStopCommand(1.0)
+                        .WithTimeout((int)(1 / TICK_TIME_SEC))
+                ).OnFalse(
+                     Motor1.RunStopCommand(-1.0)
+                        .WithTimeout((int)(1 / TICK_TIME_SEC))
+                );
+
+                /* hold START to run drivetrain in a square */
+                gamepad.Triggers[(int)Gamepad.ButtonIDs.START].WhileTrue(
+                    new SequentialCommandGroup([
+                        new RunCommand(() => {
+                            DriveSub.Drive(1.0, 0.0);
+                        }, [DriveSub]).WithTimeout((int)(1 / TICK_TIME_SEC)),
+                        new RunCommand(() => {
+                            DriveSub.Drive(0.0, 1.0);
+                        }, [DriveSub]).WithTimeout((int)(1 / TICK_TIME_SEC)),
+                        new RunCommand(() => {
+                            DriveSub.Drive(-1.0, 0.0);
+                        }, [DriveSub]).WithTimeout((int)(1 / TICK_TIME_SEC)),
+                        new RunCommand(() => {
+                            DriveSub.Drive(0.0, -1.0);
+                        }, [DriveSub]).WithTimeout((int)(1 / TICK_TIME_SEC))
+                    ]).Perpetually()
+                );
+                // NOTE: this should block the default command from running
 
                 while (true)
                 {
