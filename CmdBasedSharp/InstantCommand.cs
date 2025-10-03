7e975b02331e02b02a84741fe2936478800ef37d
Vinh Nguyen <ngtv0404@gmail.com>
2025-10-03 14:53:32 +1000

    fix InstantCommand interruptibility

--- CmdBasedSharp/InstantCommand.cs
+++ CmdBasedSharp/InstantCommand.cs
@@ -4,18 +4,18 @@ namespace CmdBased
     {
         private Action InitFunction;
 
-        private bool IsInterruptibleFlag;
         public override bool IsInterruptible
         {
-            get { return IsInterruptibleFlag; }
+            get { return true; }
         }
+        // NOTE: InstantCommand does not have an execute or end phase (and only
+        //       occupies the required subsystems briefly), so it is inherently
+        //       interruptble.
 
         public InstantCommand(Action func, 
-                              ICollection<SubsystemBase>? requirements = null,
-                              bool interruptible = true)
+                              ICollection<SubsystemBase>? requirements = null)
         {
             InitFunction = func;
-            IsInterruptibleFlag = interruptible;
             Requirements = requirements ?? [];
         }
 
